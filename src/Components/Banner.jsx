1fc94f31f7305c1c18c25d34856645c1f0ac2ee4
AnaCastroC <ana.castrocam@gmail.com>
2023-11-16 14:59:39 -0300

    [UPDATE] se agrega carrusel en banner y separación de su css en banner.css

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,64 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const BannerContainer = styled.div`
-  display: flex;
-  height: 30vw;
-  border-radius: 10px; /* Ajustar el radio de borde */
-
-  @media (max-width: 576px) {
-    flex-direction: column;
-    height: 50vw; 
-  }
-`;
-
-const LeftImage = styled.div`
-  flex: 1;
-  position: relative;
-  background: rgba(52, 152, 219, 0) url('../../../public/img/banner1.jpg') no-repeat center center;
-  background-size: 100% auto;
-  color: #ffffff;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  padding: 2vw; 
-  border-radius: 10px; 
-  overflow: hidden; 
-
-  h1 {
-    color: #ffffff;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);
-    font-size: 3vw; 
-  }
-
-  p {
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.8);
-    font-size: 2vw;
-  }
-
-  @media (max-width: 576px) {
-    border-radius: 0;
-    h1 {
-      color: #ffffff;
-      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);
-      font-size: 6vw; 
-    }
-  
-    p {
-      text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.8);
-      font-size: 4vw;
-    }
-  }
-`;
+import '@splidejs/splide/dist/css/splide.min.css';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '../assets/style/Banner.css';
 
 const Banner = () => {
   return (
-    <BannerContainer>
-      <LeftImage>
-        <h1>¡Bienvenido a Castros librería online!</h1>
-        <p>Comprando con nosotros estarás ayudando a autores chilenos</p>
-      </LeftImage>
-    </BannerContainer>
+    <Splide className="BannerContainer">
+      <SplideSlide className="Slide">
+        <img className="SlideImage" src="../../../public/img/banner1.jpg" alt="Banner 1" />
+        <div className="SlideContent">
+          <h1>¡Bienvenido a Castros librería online!</h1>
+          <p>Comprando con nosotros estarás ayudando a autores chilenos</p>
+        </div>
+      </SplideSlide>
+      <SplideSlide className="Slide">
+      <img className="SlideImage" src="../../../public/img/16elArca.jpg" alt="Banner 1" />
+        <div className="SlideContent">
+          <h1>¡Bienvenido a Castros librería online!</h1>
+          <p>Comprando con nosotros estarás ayudando a autores chilenos</p>
+        </div>
+      </SplideSlide> 
+      <SplideSlide className="Slide">
+      <img className="SlideImage" src="../../../public/img/27elJardinSecreto.jpg" alt="Banner 1" />
+        <div className="SlideContent">
+          <h1>¡Bienvenido a Castros librería online!</h1>
+          <p>Comprando con nosotros estarás ayudando a autores chilenos</p>
+        </div>
+      </SplideSlide>  
+    </Splide>
   );
 };
 
