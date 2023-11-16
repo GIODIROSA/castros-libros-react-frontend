c4f8a791f7b40ec2cd4d55d9028b6f6bfbce489f
NeaCS <naldi.castro@gmail.com>
2023-11-16 14:53:26 -0300

    [UPDATE] Cambio de carpeta el footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/style/footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="libreria-footer">Este es el footer.</footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const LayoutPublic = () => {
   return (
@@ -8,15 +9,7 @@ const LayoutPublic = () => {
         <Navbar />
         <Outlet />
       </div>
-      <footer
-        style={{
-          backgroundColor: "lightgray",
-          padding: "20px",
-          textAlign: "center",
-        }}
-      >
-        Este es el footer de mi aplicación.
-      </footer>
+      <Footer />
     </>
   );
 };
