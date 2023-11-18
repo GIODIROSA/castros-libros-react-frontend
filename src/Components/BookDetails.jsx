9c7307517e0824965c74c715db002ec1449404a4
NeaCS <naldi.castro@gmail.com>
2023-11-18 12:09:04 -0300

    [UPDATE] Se cambian las redes sociales de palabras a íconos

--- src/Components/BookDetails.jsx
+++ src/Components/BookDetails.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react';
-import ".././assets/style/bookDetails.css"
+import { useState } from "react";
+import ".././assets/style/bookDetails.css";
+import styled from "styled-components";
 
 const BookDetail = () => {
+  const Button = styled.button`
+    background-color: white;
+    color: black;
+    border: 2px solid black;
+    padding: 10px 20px;
+    font-size: 16px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    font-family: 'DM Sans', sans-serif;
+
+    @media (max-width: 768px) {
+      font-size: 14px;
+      padding: 8px 16px;
+    }
+
+    &:hover {
+      background-color: black;
+      color: white;
+    }
+  `;
   const [quantity, setQuantity] = useState(1);
 
   const handleIncrement = () => {
@@ -17,25 +39,37 @@ const BookDetail = () => {
   return (
     <div className="book-detail_container">
       <div className="book-detail">
-      <div className="book-image">
-        <img src="../../public/img/09elCodigoDaVinci.jpg" alt="titulo genérico" />
-      </div>
-      <div className="book-info">
-        <h2 className="book-title">Título genérico</h2>
-        <p className="book-price">Price: $999999</p>
-        <p className="book-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum ratione vero doloremque quo id temporibus, optio in vel quia modi? Impedit, eaque optio suscipit eveniet animi deserunt quia nihil nostrum!</p>
-        <div className="quantity-container">
-          <div className="quantity-buttons">
-            <button className="quantity-button" onClick={handleDecrement}>-</button>
-            <span className="quantity">{quantity}</span>
-            <button className="quantity-button" onClick={handleIncrement}>+</button>
-          </div>
-          <button className="add-to-cart">Agragar al carrito</button>
+        <div className="book-image">
+          <img
+            src="../../public/img/09elCodigoDaVinci.jpg"
+            alt="titulo genérico"
+          />
         </div>
-        <button className="add-to-favorites">Agregar a Favoritos</button>
+        <div className="book-info">
+          <h2 className="book-title">Título genérico</h2>
+          <p className="book-price">Price: $999999</p>
+          <p className="book-description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+            ratione vero doloremque quo id temporibus, optio in vel quia modi?
+            Impedit, eaque optio suscipit eveniet animi deserunt quia nihil
+            nostrum!
+          </p>
+          <div className="quantity-container">
+            <div className="quantity-buttons">
+              <button className="quantity-button" onClick={handleDecrement}>
+                -
+              </button>
+              <span className="quantity">{quantity}</span>
+              <button className="quantity-button" onClick={handleIncrement}>
+                +
+              </button>
+            </div>
+            <Button className= "libreria-castro__boton-agregar-carrito">Agregar al carrito</Button>
+          </div>
+          <button className="add-to-favorites">Agregar a Favoritos</button>
+        </div>
       </div>
-    </div></div>
-    
+    </div>
   );
 };
 
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,15 @@
 import ".././assets/style/footer.css";
 import { NavLink } from "react-router-dom";
+import twitterFooterIcon from "../assets/icons/twitterFooterIcon";
+import facebookFooterIcon from "../assets/icons/facebookFooterIcon";
+import linkedinFooterIcon from "../assets/icons/linkedinFooterIcon";
+import instagramFooterIcon from "../assets/icons/instagramFooterIcon";
 
 const Footer = () => {
+  const twitterFooterSvg = twitterFooterIcon()
+  const facebookFooterSvg = facebookFooterIcon()
+  const linkedinFooterSvg = linkedinFooterIcon()
+  const instagramFooterSvg = instagramFooterIcon()
   return (
     <footer className="footer">
       <div className="upper-footer">
@@ -14,10 +22,10 @@ const Footer = () => {
         <div className="container">
           <p>Todos los derechos reservados 2023</p>
           <div className="social-icons">
-            <a href="#">Linkedin</a>
-            <a href="#">Instagram</a>
-            <a href="#">Twitter</a>
-            <a href="#">Facebook</a>
+            <a href="#">{linkedinFooterSvg}</a>
+            <a href="#">{twitterFooterSvg}</a>
+            <a href="#">{facebookFooterSvg}</a>
+            <a href="#">{instagramFooterSvg}</a>
           </div>
         </div>
       </div>
