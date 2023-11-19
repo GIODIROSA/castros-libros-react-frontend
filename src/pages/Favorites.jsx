f116c28bda6faa280cac77c59ed55cd167ee58ef
AnaCastroC <ana.castrocam@gmail.com>
2023-11-19 15:45:39 -0300

    [UPDATE] se actualiza favorites.jsx

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -7,22 +7,22 @@ const Favorites = () => {
   const { books, favorites, handleToggleLike } = useFavoritesContext(); 
 
   //favoriteBooks: array con los libros favoritos filtrados desde el estado books
-  const favoriteBooks = books.filter((book) => favorites.includes(book.id)); 
+  const favoriteBooks = books.filter((book) => favorites.includes(book.producto_id)); 
 
   return (
     <div>
       <h2>Favoritos</h2>
       {favoriteBooks.map((book) => (
-        <div key={book.id}>
+        <div key={book.producto_id}>
           <img
-             src={`http://localhost:5000/uploads/${book.image}`} //verificar la ruta de la imagen del servidor
-             alt={book.name}
+             src={`http://localhost:5000/uploads/${book.producto_imagen}`} //verificar la ruta de la imagen del servidor
+             alt={book.producto_nombre}
           />
-          <h3>{book.name}</h3>
-          <p>{book.description}</p>
-          <p>Autores: {book.authors.join(', ')}</p>
-          <p>Precio: {book.price}</p>
-          <button onClick={() => handleToggleLike(book.id)}>
+          <h3>{book.producto_nombre}</h3>
+          <p>{book.producto_descripcion}</p>
+          <p>Autores: {book.producto_autores}</p>
+          <p>Precio: {book.producto_precio}</p>
+          <button onClick={() => handleToggleLike(book.producto_precio)}>
             {book.producto_favorito ? '❤️' : '🤍'} 
           </button>
         </div>
