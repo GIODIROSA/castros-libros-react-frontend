04ef0ae39244028e00e29e7b385c5f422ca1f848
AnaCastroC <ana.castrocam@gmail.com>
2023-11-26 21:06:15 -0300

    [UPDATE] se implementa logica para mandar detalle pedido al servidor

--- src/Components/DetalleCart.jsx
+++ src/Components/DetalleCart.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import "../assets/style/detalleCart.css";
 import { LibrosContext } from "../context/LibrosContext";
 import { useContext } from "react";
@@ -6,28 +7,39 @@ const DetalleCart = () => {
   const { valoresContextoLibros } = useContext(LibrosContext);
   const { carrito, incrementarProducto, decrementarProducto, totalCarrito } = valoresContextoLibros;
 
-  // const [carritoData, setCarritoData] = useState([inicial]);
+ const enviarPedidoAlServidor = async () => {
+    try {
+       // construccion del objeto de pedido con la estructura que espera el servidor
+       const pedido = {
+         pedido_fecha: new Date().toISOString(),
+         usuario_id: 2,
+         pedido_estado: true,
+         detalle_pedido: carritoFiltrado.map(item => ({
+           detalle_cantidad: item.cantidad,
+           detalle_precio_producto: item.producto_precio,
+           producto_id: item.producto_id,
+         })),
+       };
 
-  // const inicial = {
-  // {
-  //   "pedido_fecha": "2023-11-22T12:00:00Z", 
-  //   "usuario_id": 2, 
-  //   "pedido_estado": 1, 
-  //   "detalle_pedido": [
-  //     {
-  //       "detalle_cantidad": 3, 
-  //       "detalle_precio": 12000, 
-  //       "producto_id": 2 
-  //     },
-  //     {
-  //       "detalle_cantidad": 2,
-  //       "detalle_precio": 12000,
-  //       "producto_id": 3 
-  //     }
-  //   ]
-  // }
+     console.log(pedido);
 
-  //  const handlePagar = setCarritoData({}) incluir petición post;
+     // petición POST al servidor
+     const response = await axios.post('http://localhost:3001/crear_detalle_pedido', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(pedido),
+      });
+
+      if (response.ok) { // si la petición fue exitosa
+         console.log('Pedido enviado correctamente.'); // muestra un mensaje de exito en la consola
+       } else { // si la petición falla, muestra un mensaje de error en la consola
+        console.error('Error al enviar el pedido al servidor.');
+       }
+     } catch (error) { // si la petición falla, muestra un mensaje de error en la consola
+       console.error('Error en la solicitud:', error);
+     }
+   };
 
   const carritoFiltrado = carrito.filter((item) => item.cantidad > 0); // Filtra el carrito para excluir productos con cantidad cero
 
@@ -62,8 +74,8 @@ const DetalleCart = () => {
             </div>
             <div className="detalle-cart_container_button-pagar">
               <button
-                //onClick={handlePagar} 
-                className="detalle-cart_button-pagar">Pagar</button>
+                onClick={enviarPedidoAlServidor} 
+               className="detalle-cart_button-pagar">Pagar</button>
             </div>
           </div>
         </div>
